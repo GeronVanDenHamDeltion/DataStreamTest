2a777477711e474034577f01164a2b1fdc788288
Geron van den Ham <geron.ham@xs4all.nl>
2017-06-28 12:54:06 +0200

    basic system for dataentrys

---
+++ TestProject/Assets/Scripts/DataStreams.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataStreams : MonoBehaviour
+{
+    public DataHolder dataholder;
+    public List<DataHolder> streams = new List<DataHolder>();
+
+
+    public void OnTempClick()
+    {
+        streams.Add(dataholder);
+        dataholder = new DataHolder();
+    }
+}
