55c68cd1243359ce048f2b1d70d1b2b412b55970
HanHakkenberg <han.hakkenberg@gmail.com>
2017-06-28 22:48:44 +0200

    Add

--- TestProject/Assets/Scripts/DataStreams.cs
+++ TestProject/Assets/Scripts/DataStreams.cs
@@ -3,27 +3,23 @@ using System.Collections.Generic;
 using System;
 using UnityEngine;
 
-public class DataStreams : MonoBehaviour
-{
+public class DataStreams : MonoBehaviour {
     public Stream streamAdd;
     public List<DataHolder> streams = new List<DataHolder>();
     public int currentamountofstreams;
-    
+    public List<MyData> nodes = new List<MyData>();
+    public bool willem;
 
-    public void terminate(string s)
-    {
+
+    public void terminate(string s) {
         int x = Int32.Parse(s);
         print(x);
-        for (int i = 0; i < streams.Count; i++)
-        {
-            for (int o = 0; o < streams[i].stream.DataIDs.Count; o++)
-            {
-                if (streams[i].stream.DataIDs[0] == x)
-                {
+        for(int i = 0; i < streams.Count; i++) {
+            for(int o = 0; o < streams[i].stream.DataIDs.Count; o++) {
+                if(streams[i].stream.DataIDs[0] == x){
                     streams[i].stream.DataIDs.Remove(x);
                     streams[i].amount--;
-                    if (streams[i].amount == 0)
-                    {
+                    if(streams[i].amount == 0) {
                         streams.Remove(streams[i]);
                     }
                     return;
@@ -31,18 +27,24 @@ public class DataStreams : MonoBehaviour
             }
         }
     }
-    public void OnTempClick()
-    {
-        for(int i = 0; i < streams.Count; i++)
-        {
-            if (streamAdd.StartPoint == streams[i].stream.StartPoint && streamAdd.EndPoint == streams[i].stream.EndPoint)
-            {
+    public void OnTempClick() {
+        for(int i = 0; i < streams.Count; i++) {
+            if(streamAdd.StartPoint == streams[i].stream.StartPoint && streamAdd.EndPoint == streams[i].stream.EndPoint) {
                 currentamountofstreams++;
                 streams[i].stream.DataIDs.Add(currentamountofstreams);
                 streams[i].amount++;
                 streamAdd = new Stream();
+                nodes[streams[i].stream.EndPoint].IncommingData();
+                willem = false;
                 return;
             }
+            else {
+                willem = true;
+            }
+        }
+
+        if(willem == true) {
+            //nodes[streams[].stream.EndPoint].IncommingData();
         }
         DataHolder dat = new DataHolder();
         dat.stream = streamAdd;
---
+++ TestProject/Assets/Scripts/MyData.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MyData : MonoBehaviour {
+    public int myID;
+    public int myIncommingData;
+    public DataStreams ds;
+
+    public void IncommingData(){
+            print("2");
+            myIncommingData++;
+            gameObject.transform.localScale = new Vector3(myIncommingData,myIncommingData,1);
+    }
+}
